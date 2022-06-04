6ca9d7291af8a6020a6c0d4e08d8168fee90f558
Ekom Ibiok <ekom.ib@gmail.com>
2022-06-04 10:08:48 +0100

    modal-styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,9 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedItem}
     contentLabel="Selected item"
     onRequestClose={props.clearSelItem}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Selected</h3>
-    {props.selectedItem && <p>{props.selectedItem}</p>}
+    <h3 className="modal__title">You are to</h3>
+    {props.selectedItem && <p className="modal__body">{props.selectedItem}</p>}
+    <h3>Now</h3>
     <button onClick={props.clearSelItem}>Done</button>
   </Modal>
 );
